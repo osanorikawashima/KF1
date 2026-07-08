301df4090f8abe654446f69ce5fb6296899c5053
FlashingEagle <72004216+osanorikawashima@users.noreply.github.com>
2026-07-09 08:06:12 +0900

    Comment out user-defined variables in autoinstall.sh
    Comment out user-defined variables for clarity.

--- autoinstall.sh
+++ autoinstall.sh
@@ -5,9 +5,9 @@
 #echo steam steam/license note '' | sudo debconf-set-selections
 
 # Thiết lập biến
-USER_HOME="/home/ubuntu"
-GAME_DIR="$USER_HOME/kfserver"
-STEAM_CMD="/usr/games/steamcmd"
+#USER_HOME="/home/ubuntu"
+#GAME_DIR="$USER_HOME/kfserver"
+#STEAM_CMD="/usr/games/steamcmd"
 
 # 1. Cài đặt các gói cần thiết
 export NEEDRESTART_MODE=a
